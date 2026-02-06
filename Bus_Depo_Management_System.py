55ade19880c46bf8b9da04757849ee9a41345b3a
Ujjwal-Skills <ujjwalskills2025@gmail.com>
2026-02-06 17:03:30 +0530

    Added Available seats, Greeting in start, and make user input more polite

--- Bus_Depo_Management_System.py
+++ Bus_Depo_Management_System.py
@@ -1,27 +1,30 @@
+#Greetings
+print("\n||---Welcome To City Buses---||\n")
+
 #inputs from user
-start = input("Enter leaving location: ")
-end = input("Enter destination location: ")
-date = input("Enter date of journey (DD-MM-YYYY): ")
+start = input("Please enter leaving location: ")
+end = input("Please enter destination location: ")
+date = input("Please enter date of journey (DD-MM-YYYY): ")
 #time = input("Enter time of journey (HH:MM): ")
 
 #data storage
 travel_data = [
-    {"bus_number": "BUS1061", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "14:00"},
-    {"bus_number": "BUS1072", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "15:00"},
-    {"bus_number": "BUS1063", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "14:00"},
-    {"bus_number": "BUS1509", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "19:00"},
-    {"bus_number": "BUS1910", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "20:00"},
-    {"bus_number": "BUS1840", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "17:00"},
-    {"bus_number": "BUS1560", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "16:00"},
-    {"bus_number": "BUS1140", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "17:30"},
-    {"bus_number": "BUS1238", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "13:30"},
-    {"bus_number": "BUS1380", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "19:30"},
-    {"bus_number": "BUS1989", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "21:30"},
-    {"bus_number": "BUS1970", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "23:30"},
-    {"bus_number": "BUS1067", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "23:15"},
-    {"bus_number": "BUS1896", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "13:15"},
-    {"bus_number": "BUS1904", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "24:00"},
-    {"bus_number": "BUS1450", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "15:30"}    
+    {"bus_number": "BUS1061", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "14:00", "seats": "20"},
+    {"bus_number": "BUS1072", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "15:00", "seats": "20"},
+    {"bus_number": "BUS1063", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "14:00", "seats": "20"},
+    {"bus_number": "BUS1509", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "19:00", "seats": "20"},
+    {"bus_number": "BUS1910", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "20:00", "seats": "20"},
+    {"bus_number": "BUS1840", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "17:00", "seats": "20"},
+    {"bus_number": "BUS1560", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "16:00", "seats": "20"},
+    {"bus_number": "BUS1140", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "17:30", "seats": "20"},
+    {"bus_number": "BUS1238", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "13:30", "seats": "20"},
+    {"bus_number": "BUS1380", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "19:30", "seats": "20"},
+    {"bus_number": "BUS1989", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "21:30", "seats": "20"},
+    {"bus_number": "BUS1970", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "23:30", "seats": "20"},
+    {"bus_number": "BUS1067", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "23:15", "seats": "20"},
+    {"bus_number": "BUS1896", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "13:15", "seats": "20"},
+    {"bus_number": "BUS1904", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "24:00", "seats": "20"},
+    {"bus_number": "BUS1450", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "15:30", "seats": "20"}    
 ]
 
 #logic for search
@@ -34,10 +37,8 @@ for journey in travel_data:
 
 #display result
 if matches:
+    print("\n--Available Buses List:--")
     for m in matches:
-       #print("Time: "+{m['time']}+" | Route: "+ {m['start']}+" to "+ {m['end']})
-       #print()
-       #print("Available Buses List:")
-       print(f"Time: {m['time']} | Route: {m['start']} to {m['end']} | Bus Number: {m['bus_number']}")
+       print("Bus Number: "+m['bus_number']+" | Route: "+ m['start']+" to "+ m['end']+" | Time: "+m['time']+" | Available seats: "+m['seats']) 
 else:
     print("No journeys match your criteria.")

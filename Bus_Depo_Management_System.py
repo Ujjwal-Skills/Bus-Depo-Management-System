937c1afb304cf1a8cd47f1250eeaa16817b7a6c4
Ujjwal-Skills <ujjwalskills2025@gmail.com>
2026-02-06 16:41:43 +0530

    Added more buses

--- Bus_Depo_Management_System.py
+++ Bus_Depo_Management_System.py
@@ -4,28 +4,25 @@ end = input("Enter destination location: ")
 date = input("Enter date of journey (DD-MM-YYYY): ")
 #time = input("Enter time of journey (HH:MM): ")
 
-'''bus_list = [{"bus_number": "BUS101",}]
-print(bus_list)
- #"bus_number": "BUS101", "from": "CityA", "to": "CityB", "date": "15-09-2023", "time": "10:00"},
-
-routes = {
-    "CityA-to-CityB":[
-        {"date": "12-05-2024", "time": "14:00"},
-        {"date": "12-05-2024", "time": "14:30"}
-        {"date": "12-05-2024", "time": "15:00"},
-        {"date": "12-05-2024", "time": "15:30"}
-        {"date": "12-05-2024", "time": "16:00"},
-        {"date": "12-05-2024", "time": "16:30"}
-    ]
-}'''
-
 #data storage
 travel_data = [
-    {"bus_number": "BUS101", "start": "CityA", "end": "CityB", "date": "12-05-2026", "time": "14:00"},
-    {"bus_number": "BUS102", "start": "CityA", "end": "CityB", "date": "12-05-2026", "time": "15:00"},
-    {"bus_number": "BUS103","start": "CityA", "end": "CityC", "date": "12-05-2026", "time": "14:00"}
+    {"bus_number": "BUS1061", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "14:00"},
+    {"bus_number": "BUS1072", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "15:00"},
+    {"bus_number": "BUS1063", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "14:00"},
+    {"bus_number": "BUS1509", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "19:00"},
+    {"bus_number": "BUS1910", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "20:00"},
+    {"bus_number": "BUS1840", "start": "CityA", "end": "CityC", "date": "12/05/2026", "time": "17:00"},
+    {"bus_number": "BUS1560", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "16:00"},
+    {"bus_number": "BUS1140", "start": "CityA", "end": "CityB", "date": "12/05/2026", "time": "17:30"},
+    {"bus_number": "BUS1238", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "13:30"},
+    {"bus_number": "BUS1380", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "19:30"},
+    {"bus_number": "BUS1989", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "21:30"},
+    {"bus_number": "BUS1970", "start": "CityA", "end": "CityD", "date": "12/05/2026", "time": "23:30"},
+    {"bus_number": "BUS1067", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "23:15"},
+    {"bus_number": "BUS1896", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "13:15"},
+    {"bus_number": "BUS1904", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "24:00"},
+    {"bus_number": "BUS1450", "start": "CityB", "end": "CityC", "date": "12/05/2026", "time": "15:30"}    
 ]
-
 
 #logic for search
 matches = []

7fc636d38914fd806ccc989f56e36893a44f02d3
Ujjwal-Skills <ujjwalskills2025@gmail.com>
2026-02-09 18:27:33 +0530

    little change in greeting line, added price key in travel data, added seat map logic, seat selection logic, little change in ticket price calculation, added logic for adding seat number in ticket, added thanking line in end, and done little bug fix also

--- Bus_Depo_Management_System.py
+++ Bus_Depo_Management_System.py
@@ -1,5 +1,5 @@
 #Greetings
-print("\n||---Welcome To City Buses---||\n")
+print("\n||------------------------------Welcome To City Buses------------------------------||\n")
 
 #inputs from user
 start = input("Please enter leaving location: ").strip().lower()
@@ -9,22 +9,22 @@ date = input("Please enter date of journey (DD/MM/YYYY): ")
 
 #data storage
 travel_data = [
-    {"bus_number": "BUS1061", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "14:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1072", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "15:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1063", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "14:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1509", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "19:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1910", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "20:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1840", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "17:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1560", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "16:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1140", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "17:30", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1238", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "13:30", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1380", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "19:30", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1989", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "21:30", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1970", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "23:30", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1067", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "23:15", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1896", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "13:15", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1904", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "24:00", "seats": 20, "selected_seat": ["Available"]*20},
-    {"bus_number": "BUS1450", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "15:30", "seats": 20, "selected_seat": ["Available"]*20}    
+    {"bus_number": "BUS1061", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "14:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 50},
+    {"bus_number": "BUS1072", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "15:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 50},
+    {"bus_number": "BUS1063", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "14:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 45},
+    {"bus_number": "BUS1509", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "19:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 45},
+    {"bus_number": "BUS1910", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "20:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 45},
+    {"bus_number": "BUS1840", "start": "City A", "end": "City C", "date": "12/05/2026", "time": "17:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 45},
+    {"bus_number": "BUS1560", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "16:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 50},
+    {"bus_number": "BUS1140", "start": "City A", "end": "City B", "date": "12/05/2026", "time": "17:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 50},
+    {"bus_number": "BUS1238", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "13:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 70},
+    {"bus_number": "BUS1380", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "19:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 70},
+    {"bus_number": "BUS1989", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "21:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 70},
+    {"bus_number": "BUS1970", "start": "City A", "end": "City D", "date": "12/05/2026", "time": "23:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 70},
+    {"bus_number": "BUS1067", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "23:15", "seats": 20, "selected_seat": ["Available"]*20, "price": 65},
+    {"bus_number": "BUS1896", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "13:15", "seats": 20, "selected_seat": ["Available"]*20, "price": 65},
+    {"bus_number": "BUS1904", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "24:00", "seats": 20, "selected_seat": ["Available"]*20, "price": 65},
+    {"bus_number": "BUS1450", "start": "City B", "end": "City C", "date": "12/05/2026", "time": "15:30", "seats": 20, "selected_seat": ["Available"]*20, "price": 65}    
 ]
 
 #logic for search
@@ -37,53 +37,59 @@ for journey in travel_data:
 
 #display Available Buses List
 if matches:
-    print("\n--Available Buses List:--")
+    print("\n-------------Available Buses List:-------------")
     for m in matches:
        print("Bus Number: "+m['bus_number']+" | Route: "+ m['start']+" to "+ m['end']+" | Time: "+m['time']+" | Available seats: "+str(m['seats']))
 
     #Bus selection logic
     bus_selection = input("\nPlease select desired bus by entering bus number: ").strip().upper()
 
-    for bus in travel_data: 
+    for bus in matches: 
         if bus_selection == bus['bus_number']:
+            
+            #seat map and selection logic
 
-            '''#seat map and selection logic
-            available_now = bus["selected_seat"].count("Available")
-            booked_status = "XX"
             #display the visual map
-            print("+---------Seat Map for ",bus['bus_number'],"---------+")
-            print("|              Front of Bus              |")
-            print("+----------------------------------------+")
+            print("\n+---------------------------------+")
+            print("|      Seat Map for",bus['bus_number'],"      |")
+            print("+----------Front of Bus-----------+")
             for i in range(0,len(bus["selected_seat"]),4):
-                if bus["selected_seat"][i] == 'X':
-                    s1 = booked_status
-                else:
-                    s1 = "S" + str(i+1)
+                row = ""
+                for j in range(4):
+                    #if seat booked show XX else show seat number
+                    if bus["selected_seat"][i+j] == 'X':
+                        row = row + ' [XX] '
+                    else:
+                        seat_no = i + j + 1
+                        if seat_no < 10:
+                            row = row + "  [S0" + str(seat_no) + "] "
+                        else:
+                            row = row + "  [S" + str(seat_no) + "] "
+                        
+                print("|" + row + " |")
+            print("+------------Back of Bus----------+")
+            
+            #seat selection logic
+            seat_count = int(input("\nPlease enter number of seats you want to book: "))
+            if seat_count <= bus['seats']:
+                chosen = [] #store chosen seat number
                 
-                if bus["selected_seat"][i+1] == 'X':
-                    s2 = booked_status
-                else:
-                    s2 = "S" + str(i+2)
-                
-                if bus["selected_seat"][i+2] == 'X':
-                    s3 = booked_status
-                else:
-                    s3 = "S" + str(i+3)
-
-                if bus["selected_seat"][i+3] == 'X':
-                    s4 = booked_status
-                else:
-                    s4 = "S" + str(i+4)
-                
-                print("|"+ s1 + "|" + s2 + "|" + s3 + "|" + s4 +"|")
-            print("+---------------Back of Bus---------------+")'''
-        
-            seat_selection = int(input("Please enter number of seats you want to book: "))
-
-            if seat_selection <= bus['seats']:
+                #loop until user selects required seats
+                x = 0
+                while x < seat_count:
+                    seat = int(input("Please enter seat number: ")) - 1
+                    if seat < 0 or seat >= len(bus["selected_seat"]): #check seat validity
+                        print("Invalid seat number!")
+                    elif seat in chosen: #check duplicate seat entry
+                        print("You already selected this seat!")
+                    elif bus["selected_seat"][seat] == "X": #check already booked seat
+                        print("Seat is already booked!")
+                    else:
+                        chosen.append(seat)
+                        x += 1
 
                 #ticket fare logic    
-                ticket = 50*seat_selection
+                ticket = bus["price"]*seat_count
                 print("\n|  Your bill is ₹",ticket,"  |")
 
                 #ticket confirmation logic
@@ -91,9 +97,22 @@ if matches:
                 while a == 1:
                     confirmation = input("Please enter 'Confirm'/'Cancel' for booking: ").strip().lower()
                     if confirmation == 'confirm':
+                        for s in chosen:
+                            bus["selected_seat"][s] = "X"
+                        
+                        #reduce available seat count
+                        bus["seats"] = bus["seats"] - len(chosen)
+
                         print("\nYOUR SEAT IS BOOKED SUCCESSFULLY!")
-                        bus['seats'] -= seat_selection
                         a-=1
+                        
+                        #logic for adding seat number in ticket
+                        seat_string = ""
+                        for s in chosen:
+                            if s+1 < 10:
+                                seat_string = seat_string + "S0" + str(s+1) + " "
+                            else:
+                                seat_string = seat_string + "S" + str(s+1) + " "
 
                         #ticket id logic
                         ticket_id = (bus["bus_number"][3:7]+bus["start"][5]+bus["end"][5]+bus["date"][0:5])
@@ -105,22 +124,24 @@ if matches:
                         print("|                Booking Summary                  |")
                         print("+-------------------------------------------------+")
                         print("| DATE: ",bus['date'],"  | Bus No.: ",bus['bus_number'],"        |")
-                        print("| TIME: ",bus['time'],"       | Seat No.: S00             |")
+                        print("| TIME: ",bus['time'],"       | Seat No.: ",seat_string,"      |")
                         print("| FROM: ",bus['start'],"      |                           |")
                         print("| TO:   ",bus['end'],"      | PRICE: ",ticket,"              |")
                         print("+-------------------------------------------------+")
                         print("| TICKET ID: ",ticket_id,"                        |")
                         print("+-------------------------------------------------+")
 
+                        print("\n||--------------------Thanks for Traveling using City Buses--------------------||")
+
+
                     elif confirmation == 'cancel':
+                        #chosen.clear()  #clear selected seats
                         print("\nCancellation Successful!")
                         a-=1
 
-            #print("Available seats: "+ str(bus['seats']))
-
             else:
                 print("Sorry! These many seats are not available.")
-
+                #continue
 else:
     print("No journeys match your criteria.")
 
